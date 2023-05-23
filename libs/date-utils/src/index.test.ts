346b483c990bb6e4adc788f2a02ae6a3462d56d5
Abhishek Khanna <Abhishekkhanna1995@gmail.com>
2023-05-23 16:39:08 +0530

    feat: update test cases

--- libs/date-utils/src/index.test.ts
+++ libs/date-utils/src/index.test.ts
@@ -3,13 +3,13 @@ import { dateTimeToMarkdown } from '.';
 describe('date-utils', () => {
   it('should correctly format date and time to markdown in English', () => {
     const date = new Date('2023-05-18T11:30:00.000Z');
-    const expectedResult = '**Thursday, May 18, 1:30 PM**';
+    const expectedResult = '**Thursday, May 18, 5:00 PM**';
     expect(dateTimeToMarkdown(date, 'en')).toEqual(expectedResult);
   });
 
   it('should correctly format date and time to markdown in German', () => {
     const date = new Date('2023-05-18T11:30:00.000Z');
-    const expectedResult = '**Donnerstag, 18. Mai, 13:30**';
+    const expectedResult = '**Donnerstag, 18. Mai, 17:00**';
     expect(dateTimeToMarkdown(date, 'de')).toEqual(expectedResult);
   });
 });
